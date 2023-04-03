3534b5830d761ddc0f93a6b99dc5d42bca99f209
zahravaraminy <zahra.varaminy@gmail.com>
2023-04-03 11:28:06 -0400

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt=>{
+
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapControllers(); // access to folder controller
+
+using var scope=app.Services.CreateScope(); // create database
+var services=scope.ServiceProvider;
+
+try
+{
+    var context=services.GetRequiredService<DataContext>();
+    
+
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+//update data base}
+}
+catch (Exception exp)
+{
+    
+    var logger=services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(exp, "An error occured");
+}
+
+app.Run();
